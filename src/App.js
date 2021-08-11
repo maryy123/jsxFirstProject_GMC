e0d3fd7080e06299bfed0dd9a532734f526207dd
“myriam <myriam.charfeddine@etudiant-fst.utm.tn>
2021-08-11 14:12:53 +0200

    updated version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import avatar from "./avatar.jpg";
+import "./App.css";
+import "./style.css";
+let fName = "Myriam";
+let lName = "Charfeddine";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{ border: "solid 1px black", maxWidth: "100vw" }}
+          className="container"
         >
-          Learn React
-        </a>
+          <h1 className="title">
+            {fName} {lName}
+          </h1>
+
+          <img src={avatar} className="red" />
+
+          <img src="pic.jpg" className="red" />
+        </div>
+
+        <video width="320" height="240" controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
       </header>
     </div>
   );
